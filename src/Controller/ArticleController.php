742cc01d7a5c0899bfcc1ca860b2782be4f24c7a
Stephen De Ceuster <stephen@sidebury.com>
2021-03-18 13:47:23 +0100

    commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG, first homepage');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(
+            sprintf('Future page to show article: %s', $slug)
+        );
+    }
+}
