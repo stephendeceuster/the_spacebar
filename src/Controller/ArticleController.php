9d3d4d819f9d828a45caaac7030afeab19681685
Stephen De Ceuster <stephen@sidebury.com>
2021-03-24 17:51:05 +0100

    start FUNdamentals

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,22 +3,24 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('OMG, first homepage');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -30,7 +32,20 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger) {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
+
+        return $this->json(['hearts' => random_int(5, 100)]);
+
     }
 }
